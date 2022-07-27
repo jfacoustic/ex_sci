3e5f356d7d2897f3749ad0d90fc5c33519ab1cfa
Joshua Mathews <josh@32.team>
2022-07-27 09:27:28 -0600

    License, changelog, prepare package for hex

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog - ExSci
+
+### 0.0.1
+
+- Project initialization
+- Vector addition
--- mix.exs
+++ mix.exs
@@ -4,27 +4,43 @@ defmodule ExSci.MixProject do
   def project do
     [
       app: :ex_sci,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "ExSci",
+      source_url: "https://github.com/jfacoustic/ex_sci",
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    Scientific Library for Elixir
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "CHANGELOG*"],
+      maintainers: ["Joshua F. Mathews"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jfacoustic/ex_sci"}
     ]
   end
 end
